69648f596df8c75f08568c10a24b5470eb9fbe20
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-05-26 15:20:53 -0300

    Metodo Map + organizacao

---
+++ app/metodoMap.js
@@ -0,0 +1,7 @@
+function aplicaDesconto(livros) {
+    const desconto = 0.3
+    livrosDesconto = livros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+    })
+    return livrosDesconto
+}
